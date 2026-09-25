4f04ce7c89ad089878a322d2f563a3a809d2a7fb
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-25 20:23:50 +0200

    Create ft_printf_utils.c

---
+++ ft_printf_utils.c
@@ -0,0 +1,37 @@
+
+#include "ft_printf.h"
+
+int min_width()
+{
+    
+}
+
+int positive_sign()
+{
+    
+}
+
+int space()
+{
+    
+}
+
+int left_align()
+{
+    
+}
+
+int zero_padding()
+{
+    
+}
+
+int precision()
+{
+    
+}
+
+int alt_hexa()
+{
+    
+}
