42bfccd27c3ce9f01a49fced777146ce77f13966
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-25 23:29:50 +0200

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -4,20 +4,43 @@
 int ft_printf(const char *s, ...)
 {
     int count;
+    t_flags flags;
     va_list args;
     
     va_start(args, s);
     while (*s)
     {
-        s++;
+        if (*s == '%')
+        {
+            flags = parser(&s);
+            
+        }
+        else
+        {
+            putchar(s);
+            flags.print_count++;
+            s++;
+        }
     }
     va_end(args);
-    return (count);
+    return (flags.print_count);
 }
 
-int parser()
+t_flags parser(const char **s)
 {
+    t_flags flags;
     
+    while (**s != is_converter(**s))
+    {
+        if (**s == '-')
+            flags.left_padding = 1;
+        else if (**s == ' ')
+            flags.spaces = 1;
+        else if (**s == '0')
+            flags.zero_padding = 1;
+            
+        (*s)++
+    }
 }
 
 int printer_manager()
@@ -25,7 +48,16 @@ int printer_manager()
     
 }
 
-int is_converter(char c)
+char is_converter(char c)
 {
+    char *s;
     
+    s = "cspdiuxX%";
+    while (*s)
+    {
+        if (*s == c)
+            return ((char)s)
+        s++;
+    }
+    return (NULL);
 }
