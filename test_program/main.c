e21a29fd3a7293ea590ad64abe53b1cf2e17bdc4
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-25 20:47:16 +0200

    Update main.c

--- test_program/main.c
+++ test_program/main.c
@@ -8,3 +8,16 @@ int main(void)
     
     printf();
 }
+
+void test_c()
+{
+    int result_ft;
+    int result_og;
+    char c;
+    
+    c = 'W';
+    result_ft = ft_printf("no flags = %c\n", c);
+    ft_printf("%d\n", result_ft);
+    result_og = printf("no flags = %c\n", c);
+    printf("%d\n", result_og);
+}
