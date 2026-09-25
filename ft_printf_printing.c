9c6760541c21fb0b5c8d9ae84d6c93bd9ce5f967
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-25 20:20:18 +0200

    Create ft_printf_printing.c

---
+++ ft_printf_printing.c
@@ -0,0 +1,22 @@
+
+#include "ft_printf.h"
+
+int putchar(void *value)
+{
+    
+}
+
+int putnbr(void *value)
+{
+    
+}
+
+int putstr(void *value)
+{
+    
+}
+
+int puthexa(void *value)
+{
+    
+}
