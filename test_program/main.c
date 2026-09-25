ecbec33d2e3b73e2b1d9222e643553b1b208fa2c
Thomas Schwab <thomasschwab@bitminded.ch>
2026-09-25 22:47:57 +0200

    Update main.c

--- test_program/main.c
+++ test_program/main.c
@@ -4,9 +4,7 @@
 
 int main(void)
 {
-    ft_printf();
-    
-    printf();
+    test_c();
 }
 
 void test_c()
@@ -16,8 +14,21 @@ void test_c()
     char c;
     
     c = 'W';
+    ft_printf("Testing 'char c' printing:\n");
     result_ft = ft_printf("no flags = %c\n", c);
     result_og = printf("no flags = %c\n", c);
+    ft_printf("result_ft = %d\n", result_ft);
+    printf("result_og = %d\n", result_og);
+    result_ft = ft_printf("flag '-' = %-c\n", c);
+    result_og = printf("flag '-' = %-c\n", c);
+    ft_printf("result_ft = %d\n", result_ft);
+    printf("result_og = %d\n", result_og);
+    result_ft = ft_printf("flag '-' + min width 5 = %-5c\n", c);
+    result_og = printf("flag '-' + min width 5 = %-5c\n", c);
+    ft_printf("result_ft = %d\n", result_ft);
+    printf("result_og = %d\n", result_og);
+    result_ft = ft_printf("min width 5 = %5c\n", c);
+    result_og = printf("min width 5 = %5c\n", c);
     ft_printf("result_ft = %d\n", result_ft);
     printf("result_og = %d\n", result_og);
 }
